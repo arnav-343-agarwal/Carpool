d620ee4726d0ee7ede245cb7d196845e077b718d
Arnav Agarwal <agarwalarnav2003@gmail.com>
2025-03-20 14:45:39 +0530

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="py-4 text-center text-gray-600 dark:text-gray-400">
-        © {new Date().getFullYear()} Carpooling App. All rights reserved.
+        © {new Date().getFullYear()} Carpooling App. All rights reserved. Made by Arnav.
       </footer>
     );
   };
